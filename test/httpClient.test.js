fa850b783ed6e0f22144a56bbed890acd1c849bc
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2021-06-26 21:40:52 +0300

    Dockerode compose intro (#1)
    * 1st time working
    
    * refactored dockerode specific code into separate file
    
    * pull image and then spawn a container, remove afterwards
    
    * change test cmd

---
+++ test/httpClient.test.js
@@ -0,0 +1,45 @@
+const { expect, assert } = require('chai');
+const { describe, it, before, after } = require('mocha');
+const { requestTest, requestHello } = require('../src/httpClient');
+const dockerodeFacade = require('./utils/infra/dockerodeFacade');
+
+// before(function(done) {
+//   dockerodeFacade.pullImage(done);
+// });
+
+describe('REST API', () => {
+  let containerId;
+
+  before(function (done) {
+    this.timeout(60000);
+    dockerodeFacade.pullImageAndSpawnContainer(
+      done,
+      (contId) => (containerId = contId)
+    );
+  });
+
+  it('should return the expected message on the "Test" endpoint', () => {
+    requestTest()
+      .then((data) => {
+        expect(data.body.message).to.be.equal('This is a Test');
+      })
+      .catch((err) => {
+        assert.fail('expected', 'actual', err);
+      });
+  });
+
+  it('should return a greeting with the name passed to the request on the "Hello" endpoint', () => {
+    requestHello('Mocha')
+      .then((data) => {
+        expect(data.body.message).to.be.equal('Hola Mocha');
+      })
+      .catch((err) => {
+        assert.fail('expected', 'actual', err);
+      });
+  });
+
+  after(function (done) {
+    this.timeout(20000);
+    dockerodeFacade.stopAndRemoveContainer(done, containerId);
+  });
+});
---
+++ test/utils/infra/dockerodeFacade.js
@@ -0,0 +1,57 @@
+const Docker = require('dockerode');
+
+const docker = new Docker();
+
+const pullImageAndSpawnContainer = (callback, setContainerId) => {
+    docker.pull('chentex/go-rest-api', (err, stream) => {
+        docker.modem.followProgress(stream, onFinished);
+
+        function onFinished(err, _) {
+            if (err) {
+                callback(err);
+                throw err;
+            }
+            createAndStartContainer(callback, setContainerId);
+        }
+    });
+}
+
+const createAndStartContainer = (callback, setContainerId) => {
+  docker.createContainer(
+    {
+      Image: 'chentex/go-rest-api',
+      name: 'int-test-co',
+      Tty: true,
+      ExposedPorts: { '8080/tcp': {} },
+      PortBindings: { '8080/tcp': [{ HostPort: '8080' }] }
+    },
+    (err, container) => {
+      if (err) {
+        callback(err);
+      }
+      console.log(`created container with id ${container.id}`);
+      container.start((err, data) => {
+        callback();
+        setContainerId(container.id);
+      });
+    }
+  );
+};
+
+const stopAndRemoveContainer = (callback, containerId) => {
+  const container = docker.getContainer(containerId);
+  container.stop((err, _) => {
+    if (err) {
+      console.error(`container with id ${container.id} coulnd't be stopped`);
+    }
+    container.remove((err, _) => {
+      console.log(`container with id ${container.id} is removed`);
+      if (err) {
+        callback(err);
+      }
+      callback();
+    });
+  });
+};
+
+module.exports = { pullImageAndSpawnContainer, stopAndRemoveContainer };
